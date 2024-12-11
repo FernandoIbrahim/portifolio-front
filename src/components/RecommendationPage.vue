f7b53ddc507aedef7adf17c41f7ee70191a34bb3
Fernando Antônio Ferreira Ibrahim <80763509+FernandoIbrahim@users.noreply.github.com>
2024-12-11 18:34:53 -0300

    atualizaçao curriculo

--- src/components/RecommendationPage.vue
+++ src/components/RecommendationPage.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="w-full flex flex-col items-center">
         <TitleComponent content="Trabalhos"></TitleComponent>
+        <ProjectComponent name = "ArcelorMittal Sistemas" desc = "Estágio em Gerenciamento de Projetos de TI, com atuação em atividades como levantamento de requisitos, elaboração de quadros Kanban e matrizes de riscos, além do planejamento do backlog e das sprints dos projetos" :img = "`/arcelormittal.jpg`" data = "2024" url="/"
+        class="mt-20 max-w-[900px] flex"/>
         <ProjectComponent name = "Atacarejo Fernando" desc = "Pequena loja de atacado e varejo situada na cidade de Ponte Nova (Minas Gerais), a qual trabalha com uma grande diversidade de produtos, atendendo às demandas locais por mercadorias de qualidade a preços acessíveis. Durante a implementação da loja, trabalhei em uma série de atividades, incluindo o design do negócio, a implementação do sistema de vendas, suporte técnico, caixa e vendas." :img = "`/atacarejo.jpg`" data = "2023" url="https://www.instagram.com/atacarejofernando/"
         class="mt-20 max-w-[900px] flex"/>
         <ProjectComponent name = "Telus Intenacional" desc = "No início de 2022, trabalhei como freelancer para a Telus International, uma empresa terceirizada que oferece serviços para grandes empresas de tecnologia (BIGTECHs). Na posição de Social Media Evaluator, minhas responsabilidades incluíam fornecer avaliações humanas, em inglês, sobre postagens reportadas em uma grande rede social." :img = "`/telus.jpg`" data = "2023" url="https://www.telusinternational.com/solutions/ai-data-solutions/data-validation-services?linkname=data_validation_services&linktype=mainnav"
